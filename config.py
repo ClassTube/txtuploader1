c71032b0e0374582bcd65354599159028d0b2cfe
HackerADbro <150064036+HackerADbro@users.noreply.github.com>
2023-11-06 23:45:52 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6818261867:AAFhXboqTtBzh7vl1Mh05B9z0VZZGHsC9iU")
+    API_ID = int(os.environ["API_ID", 27060147]
+    API_HASH = os.environ["API_HASH", "70f93c98c61a40f69c0f830bd4fece7c"]
+    AUTH_USERS = "6693536228 """
+
